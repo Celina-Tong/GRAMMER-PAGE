71e789d01fd42bade6faefef4ce7351817ae39af
Celina Tong <celinanatong@gmail.com>
2023-08-03 10:28:07 -0700

    Initial Commit

---
+++ blossomwords-grammar/blossomwords_grammarApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossomwords_grammarApp.swift
+//  blossomwords-grammar
+//
+//  Created by Celina Tong on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct blossomwords_grammarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blossomwords-grammarUITests/blossomwords_grammarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  blossomwords_grammarUITestsLaunchTests.swift
+//  blossomwords-grammarUITests
+//
+//  Created by Celina Tong on 8/3/23.
+//
+
+import XCTest
+
+final class blossomwords_grammarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
