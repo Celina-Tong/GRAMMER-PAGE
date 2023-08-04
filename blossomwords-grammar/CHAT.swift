5d33f9f085b0a68fba5ce7215f2de5ac162222a1
Celina Tong <celinanatong@gmail.com>
2023-08-03 23:06:48 -0700

    prompt, english translation, and additional information

---
+++ blossomwords-grammar/CHAT.swift
@@ -0,0 +1,20 @@
+//
+//  CHAT.swift
+//  blossomwords-grammar
+//
+//  Created by Celina Tong on 8/3/23.
+//
+
+import SwiftUI
+
+struct CHAT: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CHAT_Previews: PreviewProvider {
+    static var previews: some View {
+        CHAT()
+    }
+}
--- blossomwords-grammar/ContentView.swift
+++ blossomwords-grammar/ContentView.swift
@@ -4,23 +4,82 @@
 //
 //  Created by Celina Tong on 8/3/23.
 //
+//               (Color("YELLOW"))
+
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+struct ContentView: View { //36
+    @State private var textTitle = ""
+    @State private var secondTextTitle = ""
+    @State private var thirdTextTitle = ""
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var body: some View { //34
+        NavigationView { //24
+            ZStack{
+                (Color("YELLOW"))
+                    .ignoresSafeArea()
+                    .overlay(
+                VStack { //22
+                    Text("Grammer")
+                        .font(.title)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.center)
+                    Group{
+                        Text(textTitle)
+                        Button("Prompt Formula") {
+                            textTitle = ("Subject-Verb: 我吃。 Wǒ chī.")
+                            
+                        }
+                        .fontWeight(.bold)
+                        .font(.title2)
+                        .foregroundColor(Color.black)
+                        .padding()
+                        .background(Color("PINK"))
+                        .cornerRadius(15)
+                        
+                        Text(secondTextTitle)
+                        Button("English Translation") {
+                            secondTextTitle = ("I eat.")
+                        }
+                        .fontWeight(.bold)
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                        .padding()
+                        .background(Color("PINK"))
+                        .cornerRadius(15)
+                        
+                        Text(thirdTextTitle)
+                        Button("Learn More About The Prompt") {
+                            thirdTextTitle = ("For many basic sentences in Chinese, it’s similar to English’s format. Subject: 我 (wǒ) = I & Verb: 吃(chī) = Eat.")
+
+                        }
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.bold)
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                        
+                        NavigationLink(destination: CHAT()) { //20
+                            Text("Try it in a conversation")
+                                .font(.title3)
+                                .foregroundColor(Color.black)
+                            
+                        }
+                    }
+                    .padding()
+                }
+                )
+            }
+        }
     }
-}
+
+        struct ContentView_Previews: PreviewProvider { //32
+            static var previews: some View { //30
+                ContentView()
+            }
+            
+        }
+        
+    }
+
